cc30ac66b3c1380225c4cf7b183578f22b0dfb01
Kirill Matrosov <root@kPC>
2016-03-16 18:33:20 +0300

    IndexRoute

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,10 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
-
-const Greeting = () => {
-    return (
-        <div>Hey there!</div>
-    )
-};
+import Posts from './components/posts_index';
 
 export default (
     <Route path="/" component={App}>
-        <Route path="greet" component={Greeting}>
-
-        </Route>
+        <IndexRoute component={Posts}></IndexRoute>
     </Route>
 );
