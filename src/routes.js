cec5b3d0202c2c9ff866f92a90b7a2015fe54a28
Kirill Matrosov <root@kPC>
2016-03-16 18:12:43 +0300

    children routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,16 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+    return (
+        <div>Hey there!</div>
+    )
+};
+
 export default (
-    <Route path="/" component={App}/>
+    <Route path="/" component={App}>
+        <Route path="greet" component={Greeting}>
+
+        </Route>
+    </Route>
 );
