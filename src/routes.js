9436264fd46448e0f8c37d26ec5a9d10a0c05d76
Kirill Matrosov <root@kPC>
2016-03-16 19:46:33 +0300

    add posts/new component

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import Posts from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 export default (
     <Route path="/" component={App}>
-        <IndexRoute component={Posts}></IndexRoute>
+        <IndexRoute component={Posts}>
+
+        </IndexRoute>
+        <Route path="posts/new" component={PostsNew}></Route>
     </Route>
 );
