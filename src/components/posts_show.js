4170ab6b47361f2370a3f0b6737efbd4aab97bdd
Kirill Matrosov <root@kPC>
2016-03-17 15:59:13 +0300

    delete action

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,23 +9,27 @@ class PostsShow extends Component {
         this.props.fetchPost(this.props.params.id);
     }
 
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id);
+    }
+
     render() {
         const {post} = this.props;
-        if (!post || post.id != this.props.params.id){
+        if (!post || post.id != this.props.params.id) {
             return (<div>Loading....</div>)
         }
         return (
             <div>
-                <Link to="/" >Back To Posts</Link>
-                <h3>{post.title}</h3>
-                <h6>Categories:{post.categories}</h6>
+                <Link to="/">Back To Posts</Link>
+                <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick}>Delete</button>
+                <h2>{post.title}</h2>
+                <h3>Categories:{post.categories}</h3>
                 <p>{post.content}</p>
             </div>
         );
     }
 }
-function mapStateToProps(state)
-{
+function mapStateToProps(state) {
     return {post: state.posts.post};
 }
 export default connect( mapStateToProps, {deletePost, fetchPost})(PostsShow);
